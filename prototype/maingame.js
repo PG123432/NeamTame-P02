96764b5b9a63364a52043a64e41e36a1c90bbd88
DavidASC20 <djchong24@gmail.com>
2022-03-18 08:42:46 -0400

    added button to spanw moles

--- prototype/maingame.js
+++ prototype/maingame.js
@@ -6,33 +6,63 @@ let lastHole;                                           //the last hole(prevents
 let timeUp = false;                                     //checks if time is up
 let score = 0;                                          //score of the user
 var requestID;
-var mole = new Image(50, 50)
 
+
+
+
+
+
+
+
+var mole = new Image(50, 50);
+mole.src = "mole.jpeg";
+var holes = new Image(c.width, c.height);
+holes.src = "holes.png";
 
 var clear = (e) => {
   console.log("clear invoked...")
   ctx.clearRect(0, 0, c.width, c.height);
   // YOUR CODE HERE
 };
+let x, y;
 
-function peep() {                                       //function to make the mole peep
+let moleButton = document.getElementById("spawnMole");
 
-  const time = randomTime(200, 1000);
-  const hole = randomHole(holes);
-  console.log(hole.name);
-  console.log(requestID);
+
+let setHole = (e) => {
+  hole = randomHole(holeObj);
+  x = hole.xcord;
+  y = hole.ycord;
   window.cancelAnimationFrame(requestID);
-
-  clear();
-  ctx.beginPath();
-  ctx.drawImage(mole, hole.xcord, hole.ycord, mole.width, mole.length);
-  setTimeout(() => {                                  //keeps going, sets new time
-    if (!timeUp) peep();
-  }, time);
+  moleSpawn();
 }
 
 
 
+let moleSpawn = () => {
+  console.log("mole Spawned");
+  console.log(requestID);
+  window.cancelAnimationFrame(requestID);
+  
+  clear();
+  ctx.beginPath();
+  ctx.drawImage(mole, x, y, mole.width, mole.height);
+  ctx.rotate(Math.PI / 2);
+ 
+  if (whack) {
+    stopIt();
+  }
+
+  requestID = window.requestAnimationFrame(moleSpawn);
+};
+
+var stopIt = () => {
+  console.log("stopIt invoked...")
+  console.log( requestID );
+  window.cancelAnimationFrame(requestID);
+};
+
+moleButton.addEventListener("click", setHole);
 
 
 
@@ -54,9 +84,7 @@ function peep() {                                       //function to make the m
 
 
 
-
-
-let holes = [
+let holeObj = [
   {
     "name": "hole1",
     "xcord": 125,
@@ -96,9 +124,8 @@ function startGame() {                                //starts the game, resets 
   timeUp = false;
   score = 0;
   // attempting to draw the holes
-  var ctx = c.getContext("2d");                           //gives context
-  var img = document.getElementById("holes");
-  void ctx.drawImage(image, 0, 0);
+  
+  ctx.drawImage(holes, 0, 0);
 
   peep();
   setTimeout(() => timeUp = true, 10000)
